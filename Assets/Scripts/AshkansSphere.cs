bd584797d82a30feafe0cb33939b22c42f8b0401
ashkanmoradi <asorrow1214@gmail.com>
2019-06-19 14:34:26 +0200

    Gamestop Condition for Falling Down
    implemented Gamestop Condition in AshkansSphere script

--- Assets/Scripts/AshkansSphere.cs
+++ Assets/Scripts/AshkansSphere.cs
@@ -3,10 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody))]
 public class AshkansSphere : MonoBehaviour
 {
-
     public float moveForce = 10f;
 
 
@@ -14,14 +14,14 @@ public class AshkansSphere : MonoBehaviour
     private Vector3 jump;
     public float jumpForce = 2.0f;
     public bool isGrounded;
-    
+
     // Start is called before the first frame update
-    
-    
+
+
     private void Awake()
     {
         myBody = GetComponent<Rigidbody>();
-        jump = new Vector3(0,2f,0);
+        jump = new Vector3(0, 2f, 0);
     }
 
 
@@ -32,21 +32,29 @@ public class AshkansSphere : MonoBehaviour
 
 
     void moveAshkansSphere()
-    {
+    {    
+        //ButtonMapping for Directional Buttons
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        
-        myBody.AddForce(new Vector3(h * moveForce, 0f, v * moveForce ));
-        
-        
+
+        myBody.AddForce(new Vector3(h * moveForce, 0f, v * moveForce));
     }
+
     void Update()
     {
-      moveAshkansSphere();
-      if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded)
-      {
-          myBody.AddForce(jump * jumpForce, ForceMode.Impulse);
-          isGrounded = false;
-      }
+        //Condition For GameStop
+        if (transform.position.y < -2)
+        {
+            Time.timeScale = 0f;
+        }
+
+        //Jumping Mechanic with Space
+        moveAshkansSphere();
+        if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        {
+            myBody.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
+
